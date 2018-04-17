52a116ac64f3c545239617002c06e7feea55d8c3
SalathielGenese <salathielgenese@gmail.com>
2018-04-17 05:42:59 +0100

    corrected according to client spec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    // AboutPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -26,7 +26,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    // AboutPage,
     ContactPage,
     HomePage,
     TabsPage
