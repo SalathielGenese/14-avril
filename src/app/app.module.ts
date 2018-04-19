d4f73dd77e8581b0835d9f99ff30d0817a5b35ea
SalathielGenese <salathielgenese@gmail.com>
2018-04-19 14:30:12 +0100

    added save contact feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+// import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { Contacts } from "@ionic-native/contacts";
 
 @NgModule({
   declarations: [
@@ -34,6 +37,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+
+    Contacts,
+    LocalNotifications,
+
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+
+
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +11,48 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private localNotifications: LocalNotifications,
+    public navCtrl: NavController,
+    private contacts: Contacts,
+  ) {
 
   }
 
+  public saveDevelopperContact()
+  {
+    console.log('.....');
+    
+    let contact: Contact = this.contacts.create();
+    contact.name = new ContactName(null, 'YIMGA YIMGA', 'Salathiel Genèse');
+    contact.emails = [
+      new ContactField('email', 'salathielgenese@gmail.com'),
+    ];
+    contact.phoneNumbers = [
+      new ContactField('mobile', '00237 682 95 77 97', true),
+    ];
+    contact.organizations = [
+      {name: 'Squall.IO', title: 'CEO'},
+      {name: 'Leaders\' Printing House', title: 'Manager'},
+      {name: 'Station Infini Web inc.', title: 'Dual-end Developper'},
+    ];
+
+    contact.save().then(() =>
+    {
+      this.localNotifications.schedule({
+        trigger: {at: new Date(new Date().getTime() + 500)},
+        text: '@SalathielGenese added to contacts',
+        led: 'FF0000',
+        sound: null
+      });
+    }, () =>
+    {
+      this.localNotifications.schedule({
+        text: 'Error : cannot add @SalathielGenese to contacts',
+        trigger: {at: new Date(new Date().getTime() + 500)},
+        led: 'FF0000',
+        sound: null
+      });
+    });
+  }
 }
